282b59975f5fcbd827b43cd8d9dd53a8d512c809
Antoneta Gjeka <antoneta.gjeka@ericsson.com>
2025-01-16 14:00:53 +0100

    Deleted FilterData.py

--- FilterData.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-#Load the file into the dataframe
-df = pd.read_excel('assets.xlsx')
-#Filter the dataframe accordingly
-df = df[(df['Item']=='Laptop') & (df['Legal Entity']=='EDD') & (df['Asset Status']=='End of Life')]
-df.to_excel('filtered_data.xlsx', sheet_name='Filtered Data from Assets')
